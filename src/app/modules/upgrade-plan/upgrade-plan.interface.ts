48caa2f13241b9f66b979d239d28dd6821ee9e30
Yeasin Arafat <leoarafat10@gmail.com>
2024-07-30 11:26:13 +0600

    [DONE] create upgrade plan module and interface

---
+++ src/app/modules/upgrade-plan/upgrade-plan.interface.ts
@@ -0,0 +1,17 @@
+import { Schema } from 'mongoose';
+
+type SubscriptionStatus = 'paid' | 'unpaid' | 'trial';
+type SubscriptionPlan = 'Gold' | 'Platinum' | 'Diamond';
+type SubscriptionState = 'active' | 'inactive';
+
+export type ISubscription = {
+  user_id: Schema.Types.ObjectId;
+  plan_id: Schema.Types.ObjectId;
+  startDate: Date;
+  endDate: Date;
+  payment_status: SubscriptionStatus;
+  plan_type: SubscriptionPlan;
+  status: SubscriptionState;
+  transactionId?: string;
+  amount: number;
+};

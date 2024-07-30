2701f3c70a553c0f5bb9ae7e76216fce28ffde60
Yeasin Arafat <leoarafat10@gmail.com>
2024-07-30 14:35:41 +0600

    [DONE] complete to product routes

--- src/app/modules/products/products.routes.ts
+++ src/app/modules/products/products.routes.ts
@@ -11,5 +11,25 @@ router.post(
   uploadFile(),
   ProductController.insertIntoDB,
 );
-
+router.get(
+  '/my-products',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.USER),
+  ProductController.myProducts,
+);
+router.get(
+  '/products',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.USER),
+  ProductController.products,
+);
+router.patch(
+  '/edit/:id',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.USER),
+  uploadFile(),
+  ProductController.updateProduct,
+);
+router.delete(
+  '/delete/:id',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.USER),
+  ProductController.deleteProduct,
+);
 export const ProductRoutes = router;

b0407bfcc7392e8724807e27eaee2d98911e526a
Yeasin Arafat <leoarafat10@gmail.com>
2024-07-30 14:34:31 +0600

    [DONE] complete to product controller

--- src/app/modules/products/products.controller.ts
+++ src/app/modules/products/products.controller.ts
@@ -18,7 +18,51 @@ const insertIntoDB = catchAsync(async (req: Request, res: Response) => {
     data: result,
   });
 });
-
+const updateProduct = catchAsync(async (req: Request, res: Response) => {
+  const result = await ProductService.updateProduct(req);
+  sendResponse<IProducts>(res, {
+    statusCode: 200,
+    success: true,
+    message: 'Product update successfully',
+    data: result,
+  });
+});
+const deleteProduct = catchAsync(async (req: Request, res: Response) => {
+  const result = await ProductService.deleteProduct(req.params.id);
+  sendResponse<IProducts>(res, {
+    statusCode: 200,
+    success: true,
+    message: 'Product delete successfully',
+    data: result,
+  });
+});
+const products = catchAsync(async (req: Request, res: Response) => {
+  const result = await ProductService.products(req.query);
+  sendResponse<IProducts[]>(res, {
+    statusCode: 200,
+    success: true,
+    message: 'Product Retrieved successfully',
+    meta: result.meta,
+    data: result.data,
+  });
+});
+const myProducts = catchAsync(async (req: Request, res: Response) => {
+  const result = await ProductService.myProducts(
+    req.user as JwtPayload,
+    req.query,
+  );
+  sendResponse<IProducts[]>(res, {
+    statusCode: 200,
+    success: true,
+    message: 'Product Retrieved successfully',
+    meta: result.meta,
+    data: result.data,
+  });
+});
 export const ProductController = {
   insertIntoDB,
+  products,
+  myProducts,
+  updateProduct,
+  deleteProduct,
 };

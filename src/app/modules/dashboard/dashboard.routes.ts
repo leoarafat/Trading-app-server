329d1cc52041fd576576e7938cafcad620f077e9
Yeasin Arafat <leoarafat10@gmail.com>
2024-07-30 10:41:46 +0600

    [DONE] complete dashboard routes

---
+++ src/app/modules/dashboard/dashboard.routes.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+import auth from '../../middlewares/auth';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import { DashboardController } from './dashboard.controller';
+const router = Router();
+router.get(
+  '/total-count',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
+  DashboardController.totalCount,
+);
+export const DashboardRoutes = router;
